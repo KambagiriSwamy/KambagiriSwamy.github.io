3d8eaae0d259fbfadb2c7f21f3b0f1f4ea6faf9e
Kambagiri Boya <bkambagiri@prokarma.com>
2018-12-03 19:38:59 +0530

    added slides

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -14,6 +15,7 @@ import { UiModule } from './ui/ui.module';
   ],
   imports: [
     BrowserModule,
+    CarouselModule.forRoot(),
     AppRoutingModule,
     SharedModule,
     EmployeeModule,
